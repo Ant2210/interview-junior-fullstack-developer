c42fd58564703df0261913d9bebcf16d6a6eb729
Ant <ant.griffiths22@gmail.com>
2023-08-01 17:13:13 +0100

    Get data from backend & enable cors on backend

--- interview-backend/src/main.ts
+++ interview-backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // This is a security risk, but it's fine for this exercise.
   await app.listen(3000);
 }
 bootstrap();
